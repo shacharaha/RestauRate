cd5522657da8784d6858b6fb6ca8babc68540940
shacharaha <shacharaha@walla.com>
2016-06-05 16:51:12 +0300

    initial commit

---
+++ app/src/main/java/com/example/shachar/resturate/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.shachar.resturate;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnCreateNewRestaurant = (Button)findViewById(R.id.btn_create_new_restaurant);
+        Button btnRestaurantList = (Button)findViewById(R.id.btn_restaurant_list);
+        btnCreateNewRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewRestaurantActivity();
+            }
+        });
+        btnRestaurantList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRestaurantListActivity();
+            }
+        });
+    }
+    private void startNewRestaurantActivity()
+    {
+        Intent intent = new Intent(this, NewRestaurantActivity.class);
+        startActivity(intent);
+    }
+    private void startRestaurantListActivity()
+    {
+        Intent intent = new Intent(this, RestaurantListActivity.class);
+        startActivity(intent);
+    }
+}
